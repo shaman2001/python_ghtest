790257394aa47831d46e06d2c028a1fd7881b323
shaman2001 <v.s.kotovich@gmail.com>
2016-12-23 00:32:46 +0300

    file allocation by packages

---
+++ test/git_hub.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from fixture.application import Application
+
+
+#from selenium.webdriver.common.keys import Keys
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_GitHubScript(app):
+    success = True
+    app.open_login_page()
+    app.login(username="v.s.kotovich@gmail.com", password="qwerty123")
+    app.different_actions()
+    app.logout()
+#    assertTrue(success)
